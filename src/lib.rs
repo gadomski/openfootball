b55d571062637eb25cc14c8c07d8f2ec4a711dbc
Pete Gadomski <pete.gadomski@gmail.com>
2019-02-05 15:45:05 -0700

    Update some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -19,13 +19,15 @@ const INITIAL_ELO_RATING: i16 = 1500;
 
 /// A season of football data.
 ///
-/// Data are provided by https://github.com/openfootball/eng-england.
+/// This was set up to work with openfootball's 2018-2019 English Premier league data, available
+/// from https://github.com/openfootball/eng-england. Your mileage may vary if you try to use with
+/// other data.
 #[derive(Debug)]
 pub struct Season {
+    k: f64,
     matchdays: Vec<Matchday>,
     name: String,
     score_factor: f64,
-    k: f64,
 }
 
 /// A matchday.
