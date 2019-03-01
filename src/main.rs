244b7bfd18c4e9d80e85baa50b5dcb1997d886ff
Pete Gadomski <pete.gadomski@gmail.com>
2019-03-01 14:21:57 -0700

    Add odds interface

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,30 @@ fn main() -> Result<(), Error> {
         .subcommand(
             SubCommand::with_name("standings")
                 .about("Prints standings as CSV data")
-                .arg(infile),
+                .arg(infile.clone()),
+        )
+        .subcommand(
+            SubCommand::with_name("odds")
+                .about("Prints odds for a given matchweek")
+                .arg(infile)
+                .arg(
+                    Arg::with_name("MATCHWEEK")
+                        .help("The matchweek")
+                        .required(true)
+                        .index(2),
+                ),
         )
         .get_matches();
+    let mut writer = Writer::from_writer(io::stdout());
     if let Some(matches) = matches.subcommand_matches("standings") {
         let season = Season::from_path(matches.value_of("INFILE").unwrap())?;
-        let mut writer = Writer::from_writer(io::stdout());
         for standing in season.standings(1500, 32.)? {
             writer.serialize(standing)?;
+        }
+    } else if let Some(matches) = matches.subcommand_matches("odds") {
+        let season = Season::from_path(matches.value_of("INFILE").unwrap())?;
+        for odds in season.odds(1500, 32., matches.value_of("MATCHWEEK").unwrap().parse()?) {
+            writer.serialize(odds)?;
         }
     }
     Ok(())
