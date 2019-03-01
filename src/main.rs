ce1b28b1c52413df3619ca1495aca0102b59dd18
Pete Gadomski <pete.gadomski@gmail.com>
2019-03-01 14:19:06 -0700

    Rearrange to subcommand

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,28 @@ extern crate openfootball;
 use failure::Error;
 
 fn main() -> Result<(), Error> {
-    use clap::{App, Arg};
+    use clap::{App, Arg, SubCommand};
     use csv::Writer;
     use openfootball::Season;
     use std::io;
 
+    let infile = Arg::with_name("INFILE")
+        .help("Sets the input openfootball text file")
+        .required(true)
+        .index(1);
     let matches = App::new("openfootball")
-        .arg(
-            Arg::with_name("INFILE")
-                .help("Sets the input openfootball text file")
-                .required(true)
-                .index(1),
+        .subcommand(
+            SubCommand::with_name("standings")
+                .about("Prints standings as CSV data")
+                .arg(infile),
         )
         .get_matches();
-    let season = Season::from_path(matches.value_of("INFILE").unwrap())?;
-    let mut writer = Writer::from_writer(io::stdout());
-    for standing in season.standings(1500, 32.)? {
-        writer.serialize(standing)?;
+    if let Some(matches) = matches.subcommand_matches("standings") {
+        let season = Season::from_path(matches.value_of("INFILE").unwrap())?;
+        let mut writer = Writer::from_writer(io::stdout());
+        for standing in season.standings(1500, 32.)? {
+            writer.serialize(standing)?;
+        }
     }
     Ok(())
 }
